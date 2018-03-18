2483f0b0561bcb28cb2deea0684c40fa20333116
Renan M. Gomes <renan_battlenet@hotmail.com>
2018-03-18 17:25:06 -0300

    Conexão

--- conexao.php
+++ conexao.php
@@ -1,14 +1,27 @@
 <?php
 
-
 $host = "localhost";
 $user = "root";
 $password = "root";
 $db = "oportunidades";
 
-$mysqli = new mysqli($host,$user,$password,$db);
 
-if($mysqli->connect_error)
-	echo "Falha na conexão(".$mysqli->connect_error.")";
+function open_database(){
+	try {
+		$con = new mysqli($host,$user,$password,$db);
+		return $con;
+	} catch (Exception $e){
+		echo $e->getMessage();
+		return null;
+	}
+}
+
+function close_database($conn) {
+	try{
+		mysqli_close($con);
+	}catch(Exception $e){
+		echo $e->getMessage();
+	}
+}
 
 ?>
