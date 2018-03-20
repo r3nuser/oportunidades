67f56d1108e7749423173afdfdc7569d9bd52e0f
WillianWho <willianoliveira608@gmail.com>
2018-03-20 17:35:53 -0300

    Junção dos códigos de validação ao de validação de cep

--- index.php
+++ index.php
@@ -1,21 +1,102 @@
 <?php include("conexao.php"); ?>
+
 <html>
 <head>
-	<style rel="stylesheet" href="styles.css"></style>
+	<link REL="STYLESHEET" HREF="CSS/STYLE.CSS" TYPE="TEXT/CSS"/>
 	<meta charset="utf-8">
 	<script type="text/javascript" src="scripts_js/cep_modulo.js"></script>
 </head>
 <body>
 	<div>
-		<form>
-			<label>Nome: <input type="text" value=""></label><br>
-			<label>Telefone: <input type="text" value=""></label><br>
-			<label>E-mail: <input type="text" value=""></label><br>
-			<label>CEP: <input type="text" value="" maxlength="10" onblur="search_cep(this.value);"></label><br>
-			<label>Rua: <input type="text" value="" id="rua"></label><br>
-			<label>Bairro: <input type="text" value="" id="bairro"></label><br>
-			<label>Cidade: <input type="text" value="" id="cidade"></label><br>
+		<form name="Cadastro" action="cadastrar.php" method="POST">
+			
+			
+			
+			<!--Aviso a respeito da obrigatoriedade dos campos-->
+			
+			<p class="obrigatorio">todos os campos com "*" são obrigatórios</p></br>				
+			<!--Campo Nome-->
+			
+			<label class="formulario">Nome Completo: <font color="red">*</font></label></br>
+			<input class="entrada" type="text" name="nome" id="nome" size="30" onblur="return validarNome()"/>
+			<p class="ocultos" id="nomeIncorreto">O campo <i>nome</i> é obrigatório</p>
+			
+			</br>
+			
+			<!--Campo Telefone-->
+			
+			<label class="formulario">Telefone:<font color="red">*</font></label></br>
+			<input class="entrada" type="text" name="telefone" id="telefone" size="14" maxlength="14" onkeydown="javascript: fMasc( this, mTel );" onblur="return validarTelefone()"/>
+			<p class="ocultos" id="tel">O campo <i>telefone</i> é obrigatório</p>
+			
+			</br>
+			
+			<!--Campo Celular-->
+			
+			<label class="formulario">Celular:<font color="red">*</font></label></br>
+			<input class="entrada" type="text" name="celular" id="celular" size="14" maxlength="14" onkeydown="javascript: fMasc( this, mTel );" onblur="return validarCelular()"/>
+			<p class="ocultos" id="cel">O campo <i>celular</i> é obrigatório</p>
+			
+			</br>
+			
+			<!--Campo Data de nascimento-->
+			
+			<label class="formulario">Data de Nascimento:<font color="red">*</font></label></br>
+			<input class="entrada" type="text" name="data" id="data" onkeydown="javascript: fMasc( this, mData );" maxlength="10" onblur="return validarData()" />
+			<p class="ocultos" id="dataN">O campo <i>Data de nascimento</i> é obrigatório</p>
+			
+			</br>
+			
+			<!--Campo E-mail-->
+			
+			<label class="formulario">E-mail:<font color="red">*</font></label></br>
+			<input class="entrada" type="mail" name="email" id="email" value="" size="25" onblur="return IsEmail(document.getElementById('email').value)"/>
+			<p class="ocultos" id="ei"> O campo <i>e-mail</i> é obrigatório</p>
+			
+			</br>
+
+			<!--Campo CPF-->
+			
+			<label class="formulario">CPF:<font color="red">*</font></label></br>
+			<input class="entrada" type="text" name="CPF" id="cpf" onkeydown="javascript: fMasc( this, mCPF );" maxlength="14" onblur="return ValidarCPF(Cadastro.cpf)"/>
+			<p class="ocultos" id="cpfi">O campo <i>CPF</i> é obrigatório</p>
+			
+			</br>
+			
+			<!--Campo CEP-->
+			
+			<label class="formulario">CEP:<font color="red">*</font></label><br>
+			<input class="entrada" name="cep" type="text" value="" onkeydown="javascript: fMasc( this, mCEP );"  maxlength="10" onblur="return search_cep(this.value);"/> 
+			<p class="ocultos" id="cepI">O campo <i>CEP</i> é obrigatório</p>
+			
+			</br>
+			
+			<!--Campo Rua-->
+			
+			<label class="formulario">Rua:<font color="red">*</font></label><br>
+			<input class="entrada" type="text" onblur="return validarRua()" value="" name="rua" id="rua">
+			<p class="ocultos" id="ruaI">O campo <i>Rua</i> é obrigatório</p>
+			
+			<br>
+			
+			<!--Campo bairro-->
+			
+			<label class="formulario">Bairro:<font color="red">*</font></label></br>
+			<input class="entrada" type="text" onblur="return validarBairro()" name="bairro" value="" id="bairro">
+			<p class="ocultos" id="bairroI">O campo <i>Bairro</i> é obrigatório</p>
+			
+			<br>
+			
+			<!--Campo Cidade-->
+			
+			<label class="formulario">Cidade:<font color="red">*</font></label><br>
+			<input class="entrada" type="text" onblur="return validarCidade()" name="cidade" value="" id="cidade">
+			<p class="ocultos" id="cidadeI">O campo <i>Cidade</i> é obrigatório</p>
+			
+			<br>
+					
 		</form>
+		<input type="submit" value="Enviar" onclick="return validarAPorraToda()"/>
 	</div>
 	
 <body>
--- scripts_js/cep_modulo.js
+++ scripts_js/cep_modulo.js
@@ -10,9 +10,12 @@ function my_callback(content){
 		document.getElementById("rua").value=(content.logradouro);
 		document.getElementById("bairro").value=(content.bairro);
 		document.getElementById("cidade").value=(content.localidade);
+		return true;
 	}else{
 		clear_data();
 		alert("CEP não encontrado !");
+		document.getElementById("cepI").style.visibility = "visible";
+		return false;
 	}
 }
 // SEARCHING CEP USING ViaCEP
@@ -20,7 +23,8 @@ function search_cep(value){
 	//New var only digits
 	var cep = value.replace(/\D/g, '');
 	//Verifies if the cep have anything
-	if(cep!=""){
+	if(cep!="")
+	{
 		// regular expression for validate CEP
 		var validacep = /^[0-9]{8}$/;
 		if(validacep.test(cep)){
@@ -32,13 +36,376 @@ function search_cep(value){
 			var script = document.createElement("script");
 			script.src= "https://viacep.com.br/ws/"+cep+"/json/?callback=my_callback";
 			document.body.appendChild(script);
+			return true;
 		}else{
 			//INVALID CEP
 			clear_data();
-			alert("CEP inválido");
+			document.getElementById("cepI").style.visibility = "visible";
+			Cadastro.cep.focus();
+			return false;
 		}
 	}
 	else{
 		clear_data();
+		document.getElementById("cepI").style.visibility = "visible";
+		Cadastro.cep.focus();
+		return false;
 	}
+}
+
+
+//Fazendo a verificação se os campos estão vazios
+
+
+function validarNome()
+{
+	var nome= Cadastro.nome.value;
+
+	if(nome =="")
+	{
+		Cadastro.nome.focus();
+		document.getElementById("nomeIncorreto").style.visibility = "visible";
+		return false;
+	} 
+	
+	else
+	{
+		document.getElementById("nomeIncorreto").style.visibility = "hidden";
+		return true;
+	}
+}
+
+function validarTelefone()
+{
+	var tel= Cadastro.telefone.value;
+	
+	if(tel =="")
+	{
+	
+		Cadastro.telefone.focus();
+		document.getElementById("tel").style.visibility = "visible";
+		return false;
+		
+	}
+	
+	else
+	{
+	
+		document.getElementById("tel").style.visibility = "hidden";
+		return true;
+	}
+}
+function validarCelular()
+{
+	var cel= Cadastro.celular.value;
+	
+	if(cel =="")
+	{
+		Cadastro.celular.focus();
+		document.getElementById("cel").style.visibility = "visible";
+		return false;
+	}
+	
+	else
+	{
+		document.getElementById("cel").style.visibility = "hidden";
+		return true;
+	}
+}
+function validarData()
+{
+	var data= Cadastro.data.value;
+	
+	if(data =="")
+	{
+		
+		Cadastro.data.focus();
+		document.getElementById("dataN").style.visibility = "visible";
+		return false;
+	}
+	
+	else
+	{	
+		document.getElementById("dataN").style.visibility = "hidden";
+		return true;
+	}
+}
+function IsEmail(email)
+{
+	var exclude=/[^@\-\.\w]|^[_@\.\-]|[\._\-]{2}|[@\.]{2}|(@)[^@]*\1/;
+	var check=/@[\w\-]+\./;
+	var checkend=/\.[a-zA-Z]{2,3}$/;
+	
+	if(((email.search(exclude) != -1)||(email.search(check)) == -1)||(email.search(checkend) == -1))
+	{
+		
+		Cadastro.email.focus();
+		document.getElementById("ei").style.visibility = "visible";
+		return false;
+	}
+	
+	else 
+	{
+		document.getElementById("ei").style.visibility = "hidden";
+		return true;
+	}
+}
+function ValidarCPF(Objcpf)
+{
+	var cpf = Objcpf.value;
+// Descartando logo de inicio alguns formatos inválidos de cpf 
+
+	if(cpf =="111.111.111-11")
+	{
+		Cadastro.CPF.focus(); 
+		document.getElementById("cpfi").style.visibility = "visible";
+		return false;
+	}
+	if(cpf =="222.222.222-22")
+	{
+		Cadastro.CPF.focus(); 
+		document.getElementById("cpfi").style.visibility = "visible";
+		return false;
+	}
+	if(cpf =="333.333.333-33")
+	{
+		Cadastro.CPF.focus(); 
+		document.getElementById("cpfi").style.visibility = "visible";
+		return false;
+	}
+	if(cpf =="444.444.444-44")
+	{
+		Cadastro.CPF.focus(); 
+		document.getElementById("cpfi").style.visibility = "visible";
+		return false;
+	}
+	if(cpf =="555.555.555-55")
+	{
+		Cadastro.CPF.focus(); 
+		document.getElementById("cpfi").style.visibility = "visible";
+		return false;
+	}
+	if(cpf =="666.666.666-66")
+		{
+		Cadastro.CPF.focus(); 
+		document.getElementById("cpfi").style.visibility = "visible";
+		return false;
+	}
+	if(cpf =="777.777.777-77")
+	{
+		Cadastro.CPF.focus(); 
+		document.getElementById("cpfi").style.visibility = "visible";
+		return false;
+	}
+	if(cpf =="888.888.888-88")
+	{
+		Cadastro.CPF.focus(); 
+		document.getElementById("cpfi").style.visibility = "visible";
+		return false;
+	}
+	if(cpf =="999.999.999-99")
+	{
+		Cadastro.CPF.focus(); 
+		document.getElementById("cpfi").style.visibility = "visible";
+		return false;
+	}
+	if(cpf =="000.000.000-00")
+	{
+		Cadastro.CPF.focus(); 
+		document.getElementById("cpfi").style.visibility = "visible";
+		return false;
+	}
+				
+	exp = /\.|\-/g
+	cpf = cpf.toString().replace( exp, "" ); 
+	var digitoDigitado = eval(cpf.charAt(9)+cpf.charAt(10));
+	var soma1=0, soma2=0;
+	var vlr =11;
+
+	for(i=0;i<9;i++)
+	{
+		soma1+=eval(cpf.charAt(i)*(vlr-1));
+		soma2+=eval(cpf.charAt(i)*vlr);
+		vlr--;
+	}
+			
+	soma1 = (((soma1*10)%11)==10 ? 0:((soma1*10)%11));
+	soma2=	(((soma2+(2*soma1))*10)%11);
+
+	var digitoGerado=(soma1*10)+soma2;
+	
+	if(digitoGerado!=digitoDigitado)
+	{	
+		Cadastro.CPF.focus(); 
+		document.getElementById("cpfi").style.visibility = "visible";
+		return false;
+	}
+	
+	else
+	{
+		document.getElementById("cpfi").style.visibility = "hidden";
+		return true;
+	}
+}
+
+function validarRua()
+{
+	var rua= Cadastro.rua.value;
+	
+	if(rua =="")
+	{
+	
+		Cadastro.rua.focus();
+		document.getElementById("ruaI").style.visibility = "visible";
+		return false;
+		
+	}
+	
+	else
+	{
+	
+		document.getElementById("ruaI").style.visibility = "hidden";
+		return true;
+	}
+}
+function validarBairro()
+{
+	var bairro= Cadastro.bairro.value;
+	
+	if(bairro =="")
+	{
+	
+		Cadastro.bairro.focus();
+		document.getElementById("bairroI").style.visibility = "visible";
+		return false;
+		
+	}
+	
+	else
+	{
+	
+		document.getElementById("bairroI").style.visibility = "hidden";
+		return true;
+	}
+}
+function validarCidade()
+{
+	var cidade= Cadastro.cidade.value;
+	
+	if(cidade =="")
+	{
+	
+		Cadastro.cidade.focus();
+		document.getElementById("cidadeI").style.visibility = "visible";
+		return false;
+		
+	}
+	
+	else
+	{
+	
+		document.getElementById("cidadeI").style.visibility = "hidden";
+		return true;
+	}
+}
+
+
+function validarAPorraToda()
+{
+	var nome = validarNome();
+	var Telefone = validarTelefone();
+	var Celular = validarCelular();
+	var Data = validarData();
+	var email = IsEmail(document.getElementById('email').value);
+	var cpf = ValidarCPF(Cadastro.cpf);
+	var rua = validarRua();
+	var bairro = validarBairro();
+	var cidade = validarCidade();
+	var cep = search_cep("Cadastro.cep");
+	
+
+	if(cpf== false || email==false || nome ==false || Telefone ==false || Celular ==false || Data ==false ||cidade ==false || bairro ==false || rua ==false || cep == false)
+	{
+		alert("preencha os campos obrigatórios corretamente");
+		return false;
+	}
+	
+	else
+	{
+		return true;
+	}
+	
+}
+	
+// Adicionando  Mascaras aos campos 
+
+function fMasc(objeto,mascara) 
+{
+	obj=objeto
+	masc=mascara
+	setTimeout("fMascEx()",1)
+}
+function fMascEx() 
+{
+		obj.value=masc(obj.value)
+	}
+function mTel(tel)
+{
+	
+	tel=tel.replace(/\D/g,"")
+	tel=tel.replace(/^(\d)/,"($1")
+	tel=tel.replace(/(.{3})(\d)/,"$1)$2")
+	
+	if(tel.length == 9)
+	{
+		tel=tel.replace(/(.{1})$/,"-$1")
+	} 
+	else if (tel.length == 10) 
+	{
+		tel=tel.replace(/(.{2})$/,"-$1")
+	} 
+	else if (tel.length == 11)
+	{
+		tel=tel.replace(/(.{3})$/,"-$1")
+	} 
+	else if (tel.length == 12)
+	{
+		tel=tel.replace(/(.{4})$/,"-$1")
+	} 
+	else if (tel.length > 12) 
+	{
+		tel=tel.replace(/(.{4})$/,"-$1")
+	}
+	return tel;
+}
+function mCNPJ(cnpj)
+{
+	cnpj=cnpj.replace(/\D/g,"")
+	cnpj=cnpj.replace(/^(\d{2})(\d)/,"$1.$2")
+	cnpj=cnpj.replace(/^(\d{2})\.(\d{3})(\d)/,"$1.$2.$3")
+	cnpj=cnpj.replace(/\.(\d{3})(\d)/,".$1/$2")
+	cnpj=cnpj.replace(/(\d{4})(\d)/,"$1-$2")
+	return cnpj
+}
+function mCPF(cpf)
+{
+	cpf=cpf.replace(/\D/g,"")
+	cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
+	cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
+	cpf=cpf.replace(/(\d{3})(\d{1,2})$/,"$1-$2")
+	return cpf
+}
+function mCEP(cep)
+{
+	cep=cep.replace(/\D/g,"")
+	cep=cep.replace(/^(\d{2})(\d)/,"$1.$2")
+	cep=cep.replace(/\.(\d{3})(\d)/,".$1-$2")
+	return cep
+}
+function mData(data)
+{
+	data=data.replace(/\D/g,"")
+	data=data.replace(/(\d{2})(\d)/,"$1/$2")
+	data=data.replace(/(\d{2})(\d)/,"$1/$2")
+	return data
 }
