842878ad8522dccd9110b64439af9997592ea1d8
WillianWho <willianoliveira608@gmail.com>
2018-04-07 19:29:10 -0300

    criando uma 'session' e preparando para enviar os dados ao banco

--- ValidarComPHP.php
+++ ValidarComPHP.php
@@ -1,4 +1,6 @@
 <?php 
+session_start();
+
 //Receiving data using the post method
 
 //Personal data
@@ -14,16 +16,34 @@
 	$bairro = $_POST['bairro'];
 	$cidade = $_POST['cidade'];
 
-// convert date format
+	// convert date format to know the age of the candidate
 
-$BrazilianDateTimeFormat = DateTime::createFromFormat('d/m/Y', $data);
-$AmericanDateTimeFormat = $BrazilianDateTimeFormat->format('Y-m-d');
+	$BrazilianDateTimeFormat = DateTime::createFromFormat('d/m/Y', $data);
+	$AmericanDateTimeFormat = $BrazilianDateTimeFormat->format('Y-m-d');
 
-if(strtotime($AmericanDateTimeFormat)> strtotime('2002-12-31'))
-{	
-	echo "<script> alert('É necessário ter no mínimo 16 anos para se inscrever no oportunidES');</script>";
-	echo "<script>history.back(1);</script>";
-}
+	if(strtotime($AmericanDateTimeFormat)> strtotime('2002-12-31'))
+	{	
+		echo "<script> alert('É necessário ter no mínimo 16 anos para se inscrever no oportunidES');</script>";
+		echo "<script>history.back(1);</script>";
+	}
+
+	//convert date format before put them into a session
+	
+
+	// putting the data caught in a "session"
+
+	$_SESSION['nome']=$nome;
+	$_SESSION['data']=$AmericanDateTimeFormat;
+	$_SESSION['CPF']=$CPF;
+	$_SESSION['telefone1']=$telefone1;
+	$_SESSION['telefone2']=$telefone2;
+	$_SESSION['email']=$email;
+	$_SESSION['CEP']=$CEP;
+	$_SESSION['rua']=$rua;
+	$_SESSION['bairro']=$bairro;
+	$_SESSION['cidade']=$cidade;
+
+	
 
 //Switch -> Curso
 
@@ -50,7 +70,7 @@ if(strtotime($AmericanDateTimeFormat)> strtotime('2002-12-31'))
 //Null field verification
 
 
-function exibe($campoRecebido)
+	function exibe($campoRecebido)
 	{
 
 		return "$campoRecebido";
@@ -85,77 +105,77 @@ function exibe($campoRecebido)
 	
 
 //Showing Data
-echo"
-	<html>
-		<head>
-			<link REL='SHORTCUT ICON' HREF='icones/favicon.ico'>
-			<title>Confirmar dados</title>
-			<link REL='STYLESHEET' HREF='CSS/STYLE.CSS' TYPE='TEXT/CSS'/>
-			<script type='text/javascript' src='scripts_js/valid_data.js'></script>
-		</head>	
-		<body>
-			<center>
-				<div class='caixa'>
-					<h1>Confirme Seus Dados</h1>
-					<p class='separador'>Dados Pessoais</p>
-					Nome: ".NotNull($nome).
-					"<br>
-					Data: ".NotNull($data).
-					"<br>
-					CPF: ".NotNull($CPF).
-					"<br>".
-					//mostrando os contatos
-					"<br>
-					<p class='separador'>Contato</p>
-					Telefone/Celular01: 
-					".NotNull($telefone1).
-					"<br>
-					Telefone/Celular02 
-					".NotNull($telefone1).
-					"<br>
-					E-mail: 
-					".NotNull($email).
-					"<br>".
-					//Mostrando endereço
-					 "<p class='separador'>Endereço</p>
-					 CEP: 
-					 ".NotNull($CEP)."
-					 <br>
-					 Rua: 
-					 ".NotNull($rua)."
-					 <br>
-					 Bairro: 
-					 ".NotNull($bairro)."
-					 <br>
-					 Cidade: 	
-					 ".NotNull($cidade)."".
-					//Mostrar curso escolhido
-					 "<p class='separador'>Cursos</p> 
-					 Curso: 
-					 ".NotNull($curso).
-					 "<br>".
-					//Mostrar dados socioeconômicos
-					 "<p class='separador'>Informações Socioeconômicas</p>
-					 <br>
-					 Trabalha? 
-					".NotSelecionar($job)."
-					<br>
-					Qual sua expectativa com o curso?
-					<br>
-					".NotSelecionar($expectativa)."
-					<br>
-					<br>
-					Como ficou sabendo sobre os<br>cursos presenciais do oportunidades?
-					<br>
-					".NotSelecionar($discoveredTheCourse)."
-					<br>
-					<br>
-					<div class='bntDiv'>
-						<input onclick='Voltar()' class='bnt' type='button' value='voltar'/> <input class='bnt' type='button' value='Confirmar'/>
+	echo"
+		<html>
+			<head>
+				<link REL='SHORTCUT ICON' HREF='icones/favicon.ico'>
+				<title>Confirmar dados</title>
+				<link REL='STYLESHEET' HREF='CSS/STYLE.CSS' TYPE='TEXT/CSS'/>
+				<script type='text/javascript' src='scripts_js/valid_data.js'></script>
+			</head>	
+			<body>
+				<center>
+					<div class='caixa'>
+						<h1>Confirme Seus Dados</h1>
+						<p class='separador'>Dados Pessoais</p>
+						Nome: ".NotNull($nome).
+						"<br>
+						Data: ".NotNull($data).
+						"<br>
+						CPF: ".NotNull($CPF).
+						"<br>".
+						//mostrando os contatos
+						"<br>
+						<p class='separador'>Contato</p>
+						Telefone/Celular01: 
+						".NotNull($telefone1).
+						"<br>
+						Telefone/Celular02 
+						".NotNull($telefone1).
+						"<br>
+						E-mail: 
+						".NotNull($email).
+						"<br>".
+						//Mostrando endereço
+						 "<p class='separador'>Endereço</p>
+						 CEP: 
+						 ".NotNull($CEP)."
+						 <br>
+						 Rua: 
+						 ".NotNull($rua)."
+						 <br>
+						 Bairro: 
+						 ".NotNull($bairro)."
+						 <br>
+						 Cidade: 	
+						 ".NotNull($cidade)."".
+						//Mostrar curso escolhido
+						 "<p class='separador'>Cursos</p> 
+						 Curso: 
+						 ".NotNull($curso).
+						 "<br>".
+						//Mostrar dados socioeconômicos
+						 "<p class='separador'>Informações Socioeconômicas</p>
+						 <br>
+						 Trabalha? 
+						".NotSelecionar($job)."
+						<br>
+						Qual sua expectativa com o curso?
+						<br>
+						".NotSelecionar($expectativa)."
+						<br>
+						<br>
+						Como ficou sabendo sobre os<br>cursos presenciais do oportunidades?
+						<br>
+						".NotSelecionar($discoveredTheCourse)."
+						<br>
+						<br>
+						<div class='bntDiv'>
+							<input onclick='Voltar()' class='bnt' type='button' value='voltar'/> <input class='bnt' type='button' value='Confirmar' onclick='javascript: location.href=\"sendToDatabase.php\";'/>
+						</div>
+						<br>
 					</div>
-					<br>
-				</div>
-			</center>
-		</body>
-	</html>";
+				</center>
+			</body>
+		</html>";
 ?>
---
+++ sendToDatabase.php
@@ -0,0 +1,17 @@
+<?php
+
+session_start();
+
+	$nome = $_SESSION['nome'];
+	$data = $_SESSION['data'];	
+	$CPF = $_SESSION['CPF'];
+	$telefone1 = $_SESSION['telefone1'];
+	$telefone2 = $_SESSION['telefone2'];
+	$email = $_SESSION['email'];
+	$CEP = $_SESSION['CEP'];
+	$rua = $_SESSION['rua'];
+	$bairro  =$_SESSION['bairro'];
+	$cidade = $_SESSION['cidade'];
+
+
+?>
